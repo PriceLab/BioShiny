af679f25d20491995c64852dea95b213afab3e1f
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-02-14 14:52:11 -0800

    deploys to shinyapps.io

--- HeatmapWidget/inst/demos/HeatmapWidgetDemo1.R
+++ HeatmapWidget/inst/demos/HeatmapWidgetDemo1.R
@@ -41,7 +41,35 @@ HeatmapDemoApp = R6Class("app",
        ) # public
     ) # class
 #--------------------------------------------------------------------------------
-app <- HeatmapDemoApp$new()
-x <- shinyApp(app$ui, app$server)
-runApp(x, port=1112)
+# need this first:
+#    install_github("ropensci/iheatmapr")
+#    install_github("PriceLab/BioShiny/HeatmapWidget")
+deploy <- function()
+{
+   require(rsconnect)
+   #rsconnect::setAccountInfo(name='hoodlab',
+   #                          token='41E779ABC50F6A98036C95AEEA1A92F7',
+   #                          secret='PDVweDhzJa8ST3fu5zihMEKVcPH0cssByz7Q6rsL')
+  # setRepositories(addURLs=c(BioCsoft="https://bioconductor.org/packages/3.12/bioc",
+  #                           BioCann="https://bioconductor.org/packages/3.12/data/annotation",
+  #                           BioCexp="https://bioconductor.org/packages/3.12/data/experiment",
+  #                           BioC="https://bioconductor.org/packages/3.12/bioc",
+  #                           CRAN="https://cran.microsoft.com"),
+  #-                 graphics=FALSE)
 
+   deployApp(account="hoodlab",
+             appName="HeatmapWidgetDemo1",
+             appTitle="Heatmap shiny widget, built upon iheatmap",
+             appFiles=c("HeatmapWidgetDemo1.R"),
+             appPrimaryDoc="HeatmapWidgetDemo1.R"
+             )
+
+
+} # deploy
+#--------------------------------------------------------------------------------
+app <-  HeatmapDemoApp$new()
+if(grepl("hagfish", Sys.info()[["nodename"]]) & !interactive()){
+   runApp(shinyApp(app$ui(), app$server), port=1114)
+   } else {
+   shinyApp(app$ui(), app$server)
+   }
