a6f8ae49a7ec869f2f5b6d042c603d0d959b9cdc
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-02-14 13:02:11 -0800

    added explanatory text, ready for shinyio

--- msgBox/inst/demos/msgBoxWidgetDemo.R
+++ msgBox/inst/demos/msgBoxWidgetDemo.R
@@ -25,11 +25,22 @@ MsgBoxDemoApp = R6Class("app",
         #------------------------------------------------------------
         ui = function(){
            fluidPage(
-              wellPanel(style="width: 1000px;",
+               div(id="foo",
+                   style="margin: 20px; padding: 10px;
+                          border: 1px solid grey;
+                          border-radius: 3px;
+                          height: 120px; width: 1000px;",
+                   h5("A simple R6 class for read-only text message display, constructor calls:"),
+                   tags$ul(
+                      tags$li('msgBoxWidget$new(id="box1", title="random mixed text")'),
+                      tags$li('msgBoxWidget$new(id="box2", title="lower case", boxWidth=400, boxHeight=50,
+                              fontSize=30, backgroundColor="lightgray")'))),
+
+              wellPanel(style="width: 1000px; height: 100px;",
                 actionButton("randomTextButton", label= "Generate random Text",
-                             style="margin-top: 40px; margin-left: 200px;"),
-                actionButton("getTextButton", label= "Get Text",
-                             style="margin-top: 40px; margin-left: 200px;")),
+                             style="margin-top: 10px; margin-left: 40px;"),
+                actionButton("getTextButton", label= "Get Text (to stdout)",
+                             style="margin-top: 10px; margin-left: 40px;")),
                 private$msgBox1$ui(),
                 private$msgBox2$ui()
             )},
@@ -58,7 +69,33 @@ MsgBoxDemoApp = R6Class("app",
        ) # public
     ) # class
 #--------------------------------------------------------------------------------
+deploy <-function()
+{
+   require(rsconnect)
+   #rsconnect::setAccountInfo(name='hoodlab',
+   #                          token='41E779ABC50F6A98036C95AEEA1A92F7',
+   #                          secret='')
+   setRepositories(addURLs=c(BioCsoft="https://bioconductor.org/packages/3.12/bioc",
+                             BioCann="https://bioconductor.org/packages/3.12/data/annotation",
+                             BioCexp="https://bioconductor.org/packages/3.12/data/experiment",
+                             BioC="https://bioconductor.org/packages/3.12/bioc",
+                             CRAN="https://cran.microsoft.com"),
+                   graphics=FALSE)
+
+   deployApp(account="hoodlab",
+              appName="msgBoxDemo",
+              appTitle="msgBox Demo",
+              appFiles=c("msgBoxWidgetDemo.R"),
+              appPrimaryDoc="msgBoxWidgetDemo.R"
+              )
+
+} # deploy
+#------------------------------------------------------------------------------------------------------------------------
 app <- MsgBoxDemoApp$new()
-x <- shinyApp(app$ui, app$server)
-runApp(x, port=1112)
+
+if(grepl("hagfish", Sys.info()[["nodename"]]) & !interactive()){
+   runApp(shinyApp(app$ui(), app$server), port=1112)
+   } else {
+   shinyApp(ui, server)
+   }
 
