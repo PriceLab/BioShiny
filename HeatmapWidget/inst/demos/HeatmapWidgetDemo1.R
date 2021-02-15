a93cbe6b51e0dfa9fd0b05e3985130f8376a641d
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-02-15 13:00:33 -0800

    successfully deployed to shinyapps.io

--- HeatmapWidget/inst/demos/HeatmapWidgetDemo1.R
+++ HeatmapWidget/inst/demos/HeatmapWidgetDemo1.R
@@ -41,27 +41,26 @@ HeatmapDemoApp = R6Class("app",
        ) # public
     ) # class
 #--------------------------------------------------------------------------------
-# need this first:
-#    install_github("ropensci/iheatmapr")
-#    install_github("PriceLab/BioShiny/HeatmapWidget")
 deploy <- function()
 {
+   repos <- options("repos")[[1]]
+   stopifnot(sort(names(repos)) == c("BioCann", "BioCsoft", "CRAN"))
+   stopifnot(repos$BioCann=="https://bioconductor.org/packages/3.12/data/annotation")
+   stopifnot(repos$BioCsoft=="https://bioconductor.org/packages/3.12/bioc")
+   stopifnot(repos$CRAN=="https://cran.microsoft.com")
+
+   require(devtools)
+   install_github("ropensci/iheatmapr", force=TRUE)
+   install_github("PriceLab/BioShiny/HeatmapWidget", force=TRUE)
+
    require(rsconnect)
-   #rsconnect::setAccountInfo(name='hoodlab',
-   #                          token='41E779ABC50F6A98036C95AEEA1A92F7',
-   #                          secret='PDVweDhzJa8ST3fu5zihMEKVcPH0cssByz7Q6rsL')
-  # setRepositories(addURLs=c(BioCsoft="https://bioconductor.org/packages/3.12/bioc",
-  #                           BioCann="https://bioconductor.org/packages/3.12/data/annotation",
-  #                           BioCexp="https://bioconductor.org/packages/3.12/data/experiment",
-  #                           BioC="https://bioconductor.org/packages/3.12/bioc",
-  #                           CRAN="https://cran.microsoft.com"),
-  #-                 graphics=FALSE)
 
    deployApp(account="hoodlab",
              appName="HeatmapWidgetDemo1",
              appTitle="Heatmap shiny widget, built upon iheatmap",
              appFiles=c("HeatmapWidgetDemo1.R"),
-             appPrimaryDoc="HeatmapWidgetDemo1.R"
+             appPrimaryDoc="HeatmapWidgetDemo1.R",
+             forceUpdate=TRUE
              )
 
 
