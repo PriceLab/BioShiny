18a577f009fcc72e7c031f4700ff2e09e9d3da7a
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-02-15 08:41:10 -0800

    dataTableWidget now in DEPENDS

--- GOEnrichmentWidget/inst/demos/GOEnrichmentWidgetDemo.R
+++ GOEnrichmentWidget/inst/demos/GOEnrichmentWidgetDemo.R
@@ -45,9 +45,52 @@ GOEnrichmentWidgetDemoApp = R6Class("app",
        ) # public
     ) # class
 #--------------------------------------------------------------------------------
+# need this first:
+# options("repos")
+#                                                    CRAN
+#                            "https://cran.microsoft.com"
+#                                                 BioCsoft
+#            "https://bioconductor.org/packages/3.11/bioc"
+#                                                 BioCann
+# "https://bioconductor.org/packages/3.11/data/annotation"
+#                                                 BioCsoft
+#            "https://bioconductor.org/packages/3.12/bioc"
+#                                                  BioCann
+# "https://bioconductor.org/packages/3.12/data/annotation"
+
+
+#   BiocManager::valid()
+#   library(devtools)
+#   install_github("PriceLab/BioShiny/GOEnrichmentWidget")
+#   install_github("PriceLab/BioShiny/msgBoxWidget")
+#   install_github("PriceLab/BioShiny/dataTableWidget")
+#   this can help figure out problems:
+#      rsconnect::appDependencies()
+
+deploy <- function()
+{
+   require(rsconnect)
+
+  #setRepositories(graphics=FALSE,
+  #                addURLs=c(BioCsoft="https://bioconductor.org/packages/3.12/bioc",
+  #                          BioCann="https://bioconductor.org/packages/3.12/data/annotation"))
+
+   deployApp(account="hoodlab",
+             appName="GOEnrichmentWidgetDemo",
+             appTitle="GO Enrichment shiny Widget",
+             appFiles=c("GOEnrichmentWidgetDemo.R"),
+             appPrimaryDoc="GOEnrichmentWidgetDemo.R"
+             )
+
+
+} # deploy
+#--------------------------------------------------------------------------------
 goi <- c("MIP", "AFP", "CAMK2N2", "CALR3", "TEX19", "CLCA2", "LINC01602", "CYP4F11",
          "MIR5581", "ENTD1", "LUM", "DRD2", "ZNF560", "MMP12", "TMEM26", "AKAP4")
 app <- GOEnrichmentWidgetDemoApp$new(geneSymbols=goi)
-x <- shinyApp(app$ui, app$server)
-runApp(x, port=1112)
 
+if(grepl("hagfish", Sys.info()[["nodename"]]) & !interactive()){
+   runApp(shinyApp(app$ui(), app$server), port=1114)
+   } else {
+   shinyApp(app$ui(), app$server)
+   }
