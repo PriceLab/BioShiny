559bcb7d60ab6b42fcc37dc10b080f9e72a693c9
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-02-15 18:00:25 -0800

    bumped version, deploy configured

--- GenomeTracksWidget/inst/demos/GenomeTracksWidgetDemo.R
+++ GenomeTracksWidget/inst/demos/GenomeTracksWidgetDemo.R
@@ -37,14 +37,48 @@ GenomeTracksWidgetDemoApp = R6Class("app",
        ) # public
     ) # class
 #--------------------------------------------------------------------------------
+#   BiocManager::valid()
+#
+deploy <- function()
+{
+   repos <- options("repos")[[1]]
+   stopifnot(sort(names(repos)) == c("BioCann", "BioCsoft", "CRAN"))
+   stopifnot(repos$BioCann=="https://bioconductor.org/packages/3.12/data/annotation")
+   stopifnot(repos$BioCsoft=="https://bioconductor.org/packages/3.12/bioc")
+   stopifnot(repos$CRAN=="https://cran.microsoft.com")
+
+   require(devtools)
+   Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS=TRUE)
+   install_github("paul-shannon/igvShiny", force=TRUE)
+   install_github("PriceLab/BioShiny/igvWidget", force=TRUE)
+   install_github("PriceLab/BioShiny/GenomeTracksWidget", force=TRUE)
+
+   require(rsconnect)
+
+   deployApp(account="hoodlab",
+             appName="GOEnrichmentWidgetDemo",
+             appTitle="GO Enrichment shiny Widget",
+             appFiles=c("GOEnrichmentWidgetDemo.R"),
+             appPrimaryDoc="GOEnrichmentWidgetDemo.R",
+             forceUpdate=TRUE
+             )
+
+
+} # deploy
+#--------------------------------------------------------------------------------
 filename <- "~/github/TrenaProjectErythropoiesis/inst/extdata/genomicRegions/tbl.3.0.250000.500000.RData"
 #filename <- "../extdata/tbl-79.rows.RData"
 #filename <- "../extdata/tbl-21.rows.RData"
 #filename <- "../extdata/tbl-66.rows.RData"
 tbl <- get(load(filename))
 
-
 app <- GenomeTracksWidgetDemoApp$new(tbl)
-x <- shinyApp(app$ui, app$server)
-runApp(x, port=1118)
+
+if(grepl("hagfish", Sys.info()[["nodename"]]) & !interactive()){
+   runApp(shinyApp(app$ui(), app$server), port=1118)
+   } else {
+   shinyApp(app$ui(), app$server)
+   }
+
+
 
